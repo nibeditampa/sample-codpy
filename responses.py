7822f6c2ef9b8e9b47a4502a03eb33d8cb7545cd
nibeditampa <nibedita.mpa@gmail.com>
2025-11-26 10:13:33 +0000

    add utils, student handler,database

---
+++ responses.py
@@ -0,0 +1,6 @@
+def send_json(handler, status, data):
+    handler.send_response(status)
+    add_cors_headers(handler)
+    handler.send_header("Content-Type", "application/json")
+    handler.end_headers()
+    handler.wfile.write(json.dumps(data).encode("utf-8"))
